968696a8b768b0702475f0dfdfe3a074817e421f
AlSharp <elistaphilly@gmail.com>
2026-01-21 00:10:22 -0500

    add flags

--- src/App.jsx
+++ src/App.jsx
@@ -5,44 +5,85 @@ import AvatarEditor from 'react-avatar-editor';
 function App() {
 
   const FNL_LOGO_URL = window.location.origin + '/fnl_logo.png';
-  const SIZE = 1024;
+  const SIZE = 512;
+  const FLAG_HEIGHT = 110;
+  const FLAG_WIDTH = 220;
+  const REPUBLICS = [
+    { name: 'Ойрат-Калмыкия', image: 'oirat-kalmykia.png' },
+    { name: 'Ойрат-Калмыкия 1992', image: 'oirat-kalmykia-1992.png' },
+    { name: 'Башкортостан', image: 'bashkortostan.png' },
+    { name: 'Татарстан', image: 'tatarstan.jpg' },
+    { name: 'Бурятия', image: 'buryatia.png' },
+  ]
 
   const editorRef = useRef(null);
   const canvasRef = useRef(null);
   const logoOverlayRef = useRef(null);
+  const flagOverlayRef = useRef(null);
   const [image, setImage] = useState(null);
-  const [logo, setLogo] = useState(null);
   const [scale, setScale] = useState(1);
+  const [flag, setFlag] = useState('');
+  const [flagImage, setFlagImage] = useState(null);
 
   useEffect(() => {
-    const logo = new Image();
-    logo.src = FNL_LOGO_URL;
-    setLogo(logo);
+    drawLogo();
     if (editorRef.current) {
       canvasRef.current = editorRef.current.canvas.current;
     }
   }, []);
 
   useEffect(() => {
-    if (logo) {
-      drawLogo(logo);
+    if (flag === '') {
+      setFlagImage(null);
+    } else {
+      const flagImage = new Image();
+      flagImage.src = window.location.origin + '/' + flag;
+      flagImage.onload = () => setFlagImage(flagImage);
     }
-  }, [logo])
+  }, [flag])
 
-  const handleScale = (event) => {
+  useEffect(() => {
+    if (flagImage) {
+      const canvas = flagOverlayRef.current;
+      const ctx = canvas.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(
+        flagImage,
+        canvas.width / 2 - FLAG_WIDTH / 2,
+        canvas.height - FLAG_HEIGHT - 60,
+        FLAG_WIDTH,
+        FLAG_HEIGHT);
+    } else {
+      const canvas = flagOverlayRef.current;
+      const ctx = canvas.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  }, [flagImage])
+
+  const onScaleChange = (event) => {
     const scale = parseFloat(event.target.value);
     setScale(scale);
   }
 
-  const drawLogo = (logo) => {
-    const canvas = logoOverlayRef.current;
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(logo, 30, 300, 120 * 2, 225 * 2);
+  const onFlagChange = (event) => {
+    setFlag(event.target.value);
+  }
+
+  const drawLogo = () => {
+    const logo = new Image();
+    logo.src = FNL_LOGO_URL;
+    logo.onload = () => {
+      const canvas = logoOverlayRef.current;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(logo, 15, 150, logo.width, logo.height);
+    };
   }
 
   const clearImage = () => {
     setImage(null);
     setScale(1);
+    setFlagImage(null);
+    setFlag('');
   }
 
   const downloadAvatar = () => {
@@ -53,6 +94,7 @@ function App() {
     const ctx = exportCanvas.getContext('2d');
     ctx.drawImage(editorRef.current.getImageScaledToCanvas(), 0, 0);
     ctx.drawImage(logoOverlayRef.current, 0, 0);
+    ctx.drawImage(flagOverlayRef.current, 0, 0);
 
     exportCanvas.toBlob((blob) => {
       const url = URL.createObjectURL(blob);
@@ -83,7 +125,7 @@ function App() {
           <Dropzone
             onDrop={(files) => setImage(files[0])}
             noClick={!!image}
-            style={{ width: '250px', height: '250px' }}
+            style={{ width: 250, height: 250 }}
           >
             {({ getRootProps, getInputProps }) => (
               <div className="relative bg-white" {...getRootProps()}>
@@ -92,16 +134,23 @@ function App() {
                   image={image}
                   width={SIZE}
                   height={SIZE}
-                  borderRadius={512}
+                  borderRadius={256}
                   scale={scale}
-                  style={{ width: '250px', height: '250px' }}
+                  style={{ width: 250, height: 250 }}
                 />
                 <canvas
                   ref={logoOverlayRef}
                   className="absolute inset-0 pointer-events-none"
                   width={SIZE}
                   height={SIZE}
-                  style={{ width: '250px', height: '250px' }}
+                  style={{ width: 250, height: 250 }}
+                />
+                <canvas
+                  ref={flagOverlayRef}
+                  className="absolute inset-0 pointer-events-none"
+                  width={SIZE}
+                  height={SIZE}
+                  style={{ width: 250, height: 250 }}
                 />
                 <input {...getInputProps()} />
               </div>
@@ -118,7 +167,16 @@ function App() {
               max="2"
               step="0.01"
               value={scale}
-              onChange={handleScale} />
+              onChange={onScaleChange} />
+          </div>
+          <div className="mb-4 w-64">
+            <label htmlFor="flag">Добавьте свой флаг: </label>
+            <select name="flag" id="flag" onChange={onFlagChange} value={flag} className="w-full mt-2 bg-transparent border border-gray-300 px-4 py-2 pr-8 text-sm text-gray-800 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-gray-400">
+              <option value="">Выберите республику</option>
+              {REPUBLICS.map(rep => (
+                <option key={rep.name} value={rep.image}>{ rep.name }</option>
+              ))}
+            </select>
           </div>
           <div className="mb-4">
             <button className="w-64 py-2 bg-[#a5a56c] text-white hover:cursor-pointer font-bold" onClick={downloadAvatar}>
